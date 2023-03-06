cf49c38fd535a6e9e27ff2d916a4623a4257890a
Rafael Sousa <66565227+Rafaavinii@users.noreply.github.com>
2023-03-05 23:23:08 -0300

    Update Chat.java

--- src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
@@ -1,34 +1,85 @@
 package br.ufs.dcomp.ChatRabbitMQ;
 
 import com.rabbitmq.client.*;
-
 import java.io.IOException;
+import java.util.Scanner;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class Chat {
-
+  
+  public static String destino = "";
   public static void main(String[] argv) throws Exception {
-    ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost("ip-da-instancia-da-aws"); // Alterar
-    factory.setUsername("usuário-do-rabbitmq-server"); // Alterar
-    factory.setPassword("senha-do-rabbitmq-server"); // Alterar
-    factory.setVirtualHost("/");
-    Connection connection = factory.newConnection();
-    Channel channel = connection.createChannel();
     
-    String QUEUE_NAME = "minha-fila";
-                      //(queue-name, durable, exclusive, auto-delete, params); 
-    channel.queueDeclare(QUEUE_NAME, false,   false,     false,       null);
+      ConnectionFactory factory = new ConnectionFactory();
+      factory.setHost("18.204.20.47	"); 
+      factory.setUsername("admin");
+      factory.setPassword("password"); 
+      factory.setVirtualHost("/");
+      Connection connection = factory.newConnection();
+      Channel channel = connection.createChannel();
+      
+      Scanner input = new Scanner(System.in);
+      
+      String msg = ""; 
+
+      String QUEUE_Send = "";
     
-    Consumer consumer = new DefaultConsumer(channel) {
-      public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)           throws IOException {
+      String Exchange = "";
 
-        String message = new String(body, "UTF-8");
-        System.out.println(message);
 
+      System.out.print("usuario: "); 
+      String QUEUE_NAME = input.nextLine();
+      QUEUE_NAME = "@" + QUEUE_NAME;
+      
+      //canal para envio de mensagens
+      channel.queueDeclare(QUEUE_NAME, false, false, false, null); 
+      
+      //consumidores de mensagen
+      Consumer consumer_msg = new DefaultConsumer(channel) { 
+        public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+          String message = new String(body, StandardCharsets.UTF_8);
+          String sender = envelope.getRoutingKey();
+          SimpleDateFormat sdf = new SimpleDateFormat("(dd/MM/yyyy HH:mm)");
+          System.out.println(sdf.format(new Date()) + " " + sender + " diz: " + message);
+          System.out.print(">> ");
+        }
+      }; 
+      
+      channel.basicConsume(QUEUE_NAME,true, consumer_msg);
+      
+      System.out.print(">>");
+      msg = input.nextLine();
+      
+      while (!msg.equals("exit")) {
+        String[] msg_par = msg.split(" ");
+
+        if (msg.subSequence(0, 1).equals("@")){
+          QUEUE_Send = msg;
+          destino = QUEUE_Send;
+          Exchange = "";
+          
+        }
+        
+        else if(msg.subSequence(0, 1).equals("#")){
+          Exchange = msg.substring(1,msg.length());
+          destino = msg;
+          QUEUE_Send = "";
+        }
+        
+        System.out.print(destino + ">>");
+        msg = input.nextLine();
       }
-    };
-                      //(queue-name, autoAck, consumer);    
-    channel.basicConsume(QUEUE_NAME, true,    consumer);
-    
+      
+      channel.close();
+      connection.close();  
+      
+
   }
 }
